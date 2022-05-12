c21fc654873b3923a6d65833efb8ff3d6dbaec5c
PatriotxZero <Coreylong90@icloud.com>
2022-05-11 20:14:46 -0400

    added hooks and search options constonant

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchForm from "./components/SearchForm";
 import SearchResults from "./components/SearchResults";
 
-const images = [
-  {
-    id: 1,
-    url: 'https://media.giphy.com/media/3DsNP07nApt1eEyjvM/giphy.gif'
-  },
-  {
-    id: 2,
-    url: 'https://media1.giphy.com/media/26BRv0ThflsHCqDrG/giphy.gif'
-  },
-  {
-    id: 3,
-    url:
-      'https://www.thisiscolossal.com/wp-content/uploads/2018/04/agif2opt.gif'
-  },
-  {
-    id: 4,
-    url: 'https://media.giphy.com/media/3oEdva9BUHPIs2SkGk/giphy.gif'
-  },
-  {
-    id: 5,
-    url: 'https://media.giphy.com/media/3o6ozoD1ByqYv7ARIk/giphy.gif'
-  },
-  {
-    id: 6,
-    url: 'https://buffer.com/library/wp-content/uploads/2016/06/giphy.gif'
-  },
-  {
-    id: 7,
-    url: 'https://media4.giphy.com/avatars/default5.gif'
-  },
-  {
-    id: 8,
-    url: 'https://media2.giphy.com/media/3oEduPff5ErjNmlbwY/source.gif'
-  }
-];
 
 function App() {
+
+  const searchOptions = {
+    key: process.env.REACT_APP_GIPHY_KEY,
+    limit: 25,
+    rating: 'G',
+    api: 'https://api.giphy.com/v1/gifs',
+    endpoint: '/search'
+  }
+
+  const [images, setImages] = useState([])
+  useEffect(() => {
+    getImages()
+  }, [])
+  
   return(
     <div>
       <h1>Giphy Searcher</h1>
